f61e2e7956e477feff97a024552312cf5d9f7b5f
Arnab Banerjee <arnab.ban09@gmail.com>
2019-08-06 18:35:55 +0530

    Added Config Server app and client apps

---
+++ SpringConfigServerClientApp1/src/main/java/com/arnab/spring/cloud/api/SpringConfigServerClientApp1Application.java
@@ -0,0 +1,13 @@
+package com.arnab.spring.cloud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigServerClientApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServerClientApp1Application.class, args);
+	}
+
+}
---
+++ SpringConfigServerClientApp2/src/main/java/com/arnab/spring/cloud/api/SpringConfigServerClientApp2Application.java
@@ -0,0 +1,13 @@
+package com.arnab.spring.cloud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigServerClientApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigServerClientApp2Application.class, args);
+	}
+
+}
